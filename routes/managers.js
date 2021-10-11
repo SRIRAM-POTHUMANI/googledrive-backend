a6a58b935c50025871711fea3979620cdcd7e7cb
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-10-12 02:43:39 +0530

    cloud env

--- routes/managers.js
+++ routes/managers.js
@@ -3,8 +3,8 @@ import express from "express";
 import { MongoClient } from "mongodb";
 const router = express.Router();
 export async function createConnection() {
-  // const MONGO_URL = process.env.MONGO_URL;
-  const MONGO_URL = "mongodb://localhost/users";
+  const MONGO_URL = process.env.MONGO_URL;
+//   const MONGO_URL = "mongodb://localhost/users";
   const client = new MongoClient(MONGO_URL);
   await client.connect();
   console.log("Successfull 💚");
