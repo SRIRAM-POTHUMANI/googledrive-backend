fa5440046731b210462647afe4b12a54473cf407
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-10-12 03:21:21 +0530

    login done

--- routes/managers.js
+++ routes/managers.js
@@ -3,19 +3,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import { createConnection } from "../index.js";
 const router = express.Router();
-<<<<<<< HEAD
 
-=======
-export async function createConnection() {
-  const MONGO_URL = process.env.MONGO_URL;
-//   const MONGO_URL = "mongodb://localhost/users";
-  const client = new MongoClient(MONGO_URL);
-  await client.connect();
-  console.log("Successfull 💚");
-  // const insertData = await client.db("users").collec tion("people").insertMany(users);
-  return client;
-}
->>>>>>> eb9c66deac6fe90a5d8dcb2ef3f057c7cd5f50fe
 // get manager on local
 router.get("/", async (request, response) => {
   const client = await createConnection();
@@ -33,7 +21,7 @@ router.post("/signup", async (request, response) => {
     .insertOne({ username: username, password: hashedpassword });
   response.send(result);
 });
-<<<<<<< HEAD
+
 //manager login
 router.post("/login", async (request, response) => {
   const client = await createConnection();
@@ -45,6 +33,4 @@ router.post("/login", async (request, response) => {
   const passwordMatched = await bcrypt.compare(password,result.password);
   response.send(passwordMatched ? "login successful" : "login failed");
 });
-=======
->>>>>>> eb9c66deac6fe90a5d8dcb2ef3f057c7cd5f50fe
 export const managerRouter = router;
