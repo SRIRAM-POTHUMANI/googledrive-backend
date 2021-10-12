22626fb601c06bdfcac053aed4a3fda4a7d73160
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-10-13 03:38:09 +0530

    cors installed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { MongoClient } from "mongodb";
@@ -8,7 +10,9 @@ import { movieRouter } from "./routes/movies.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
+
 app.use(express.json());
+app.use(cors());
 
 export async function createConnection() {
   const MONGO_URL = process.env.MONGO_URL;
